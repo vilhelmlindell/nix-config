291bdb7132b5495c8d1e1a17fedc8ae1b2846325
Vilhelm <vilhelm.lindell123@gmail.com>
2024-03-13 23:52:16 +0100

    update

---
+++ home-manager/zellij.nix
@@ -0,0 +1,20 @@
+{config}:
+{
+  programs.zellij = {
+    enable = true;
+    settings = {
+      theme = "base16";
+      themes.base16.fg = "#${config.colorScheme.palette.base04}";
+      themes.base16.bg = "#${config.colorScheme.palette.base01}";
+      themes.base16.black = "#${config.colorScheme.palette.base00}";
+      themes.base16.red = "#${config.colorScheme.palette.base08}";
+      themes.base16.green = "#${config.colorScheme.palette.base0B}";
+      themes.base16.yellow = "#${config.colorScheme.palette.base0A}";
+      themes.base16.blue = "#${config.colorScheme.palette.base0D}";
+      themes.base16.magenta = "#${config.colorScheme.palette.base0E}";
+      themes.base16.cyan = "#${config.colorScheme.palette.base0C}";
+      themes.base16.white = "#${config.colorScheme.palette.base05}";
+      themes.base16.orange = "#${config.colorScheme.palette.base09}";
+    };
+  };
+}
